f737fcd84c0cf6c5af71aa953b81049e40bed80d
Slater <tbs204@exeter.ac.uk>
2026-01-28 11:04:06 +0000

    All flows defined in setup + solve function tweaked to deal with solving for longer than a delay period.

--- sd.py
+++ sd.py
@@ -1,7 +1,7 @@
 # Load require packages / files
 import numpy as np
 from scipy.integrate import solve_ivp, OdeSolution
-from functions.SDFunctions import stock_delay, check_delay_time
+from functions import SDFunctions as sdf
 
 class SDSetup:
 	
@@ -11,6 +11,7 @@ class SDSetup:
 		self.contact_rate = params['contact_rate']
 		self.infectivity = params['infectivity']
 		self.vaccine_uptake = params['vaccine_uptake']
+		self.death_prob = params['death_prob']
 
 		# Delay parameters
 		self.death_delay = params['death_delay']
@@ -18,10 +19,10 @@ class SDSetup:
 		self.immunity_delay = params['immunity_delay']
 
 		# Stocks
-		self.susceptible = np.array([initial_conditions['susceptible']])
-		self.infected = np.array([initial_conditions['infected']])
-		self.immune = np.array([initial_conditions['immune']])
-		self.dead = np.array([initial_conditions['dead']])
+		self.S = np.array([initial_conditions['susceptible']])
+		self.I = np.array([initial_conditions['infected']])
+		self.M = np.array([initial_conditions['immune']])
+		self.D = np.array([initial_conditions['dead']])
 
 		# Store timepoints
 		self.time = np.array([0])
@@ -32,7 +33,7 @@ class SDSetup:
 		S, I, M, D = y
 
 		# Calculate infection rate
-		output = self.contact_rate * self.infectivity * S * I / (S + I + H)
+		output = (self.contact_rate * self.infectivity * S * I) / (S + I + M)
 
 		return output
 
@@ -40,11 +41,62 @@ class SDSetup:
 
 		# Stock values
 		S, I, M, D = y
-
+		
 		# Calculate vaccine rate
 		output = self.vaccine_uptake * S
 
 		return output
+
+	def recovery_rate(self, t, y):
+
+		if sdf.check_delay_time(t, self.recovery_delay):
+
+			# Delayed stock 
+			y_delay = sdf.stock_delay(t, self.recovery_delay, self.interpolator)
+	
+			# Calculate recovery rate
+			output = (1-self.death_prob) * self.infection_rate(t, y_delay)
+
+			print([t, output, y, y_delay])
+
+		else: 
+			output = 0
+
+		return output	
+
+	def death_rate(self, t, y):
+
+		if sdf.check_delay_time(t, self.death_delay):
+
+			# Delayed stock 
+			y_delay = sdf.stock_delay(t, self.death_delay, self.interpolator)
+	
+			# Calculate recovery rate
+			output = self.death_prob * self.infection_rate(t, y_delay)
+
+		else: 
+			output = 0
+
+		return output
+
+	def susceptibility_rate(self, t, y):
+
+		if sdf.check_delay_time(t, self.immunity_delay):
+
+			# Delayed stock 
+			y_delay = sdf.stock_delay(t, self.immunity_delay, self.interpolator)
+
+			print(f'This is a SR calculation with delayed stock {y_delay}!')
+	
+			# Calculate recovery rate
+			output = self.vaccine_rate(t, y_delay) + self.recovery_rate(t, y_delay)
+
+			print(f'SR output is {output}.')
+
+		else: 
+			output = 0
+
+		return output	
 
 class SDSolve(SDSetup):
 
@@ -61,65 +113,48 @@ class SDSolve(SDSetup):
 
 	def stock_equations(self, t, y):
 
-		# Infection rate
-		infection_rate = self.infection_rate(t, y)
-
-		# Vaccine rate
-		vaccine_rate = self.vaccine_rate(t, y)
-
-		# Death rate
-		if check_delay_time(t, self.death_delay):
-			delayed_y = stock_delay(t, self.death_delay, self.interpolator)
-			death_rate = self.infection_rate(t, delayed_y)
-		else:
-			death_rate = 0
-
-		# Recovery rate
-		if check_delay_time(t, self.recovery_delay):
-			delayed_y = stock_delay(t, self.recovery_delay, self.interpolator)
-			recovery_rate = self.infection_rate(t, delayed_y)
-		else:
-			recovery_rate = 0
-
-		# Susceptibility rate
-		if check_delay_time(t, self.immunity_delay):
-			delayed_y = stock_delay(t, self.immunity_delay, self.interpolator)
-			susceptibility_rate = self.vaccine_rate(t, delayed_y) + \
-			self.recovery_rate(t, delayed_y)
-		else:
-			susceptibility_rate = 0
+		# Calculate flows
+		ir = self.infection_rate(t, y)
+		vr = self.vaccine_rate(t, y)
+		dr = self.death_rate(t, y)
+		rr = self.recovery_rate(t, y)
+		sr = self.susceptibility_rate(t, y)
 
 		# Stock equations
-		dSdt = susceptibility_rate - infection_rate - vaccine_rate
-		dIdt = infection_rate - death_rate - recovery_rate
-		dMdt = recovery_rate + vaccine_rate - susceptibility_rate
-		dDdt = - death_rate
+		dSdt = sr - ir - vr
+		dIdt = ir - dr - rr
+		dMdt = rr + vr - sr
+		dDdt = dr
 
 		return np.array([dSdt, dIdt, dMdt, dDdt])
 
 	def solve(self, t):
 
-		# Initial conditions
-		y0 = [self.S[-1], self.I[-1], self.M[-1], self.D[-1]]
-
-		# Time domain for solving
-		time_domain = [self.time[-1], t]
-
-		# Solve stock equations
-		solutions = solve_ivp(self.stock_equations, time_domain, y0, 
-							  t_eval=[t], dense_output=True)
-
-		# Append solutions
-		S, I, M, D = solutions.y.T
-		self.S = np.append(self.S, S)
-		self.I = np.append(self.I, I)
-		self.M = np.append(self.M, M)
-		self.D = np.append(self.D, D)
-
-		# Append interpolator
-		if self.interpolator:
-			ts = np.append(interpolator.ts, solutions.sol.ts[1:])
-			interpolants = interpolator.interpolants + solutions.sol.interpolants
-			self.interpolator = OdeSolution(ts, interpolants)
-		else: 
-			self.interpolator = solutions.sol
+		while self.time[-1] < t:
+			tmax = min(self.time[-1] + self.min_delay, t)
+			
+			# Initial conditions
+			y0 = [self.S[-1], self.I[-1], self.M[-1], self.D[-1]]
+		
+			# Time domain
+			time_domain = [self.time[-1], tmax]
+		
+			# Solve stock equations
+			solutions = solve_ivp(self.stock_equations, time_domain, y0, 
+								  dense_output=True, method='LSODA')
+		
+			S, I, M, D = solutions.y[:, -1]
+			self.S = np.append(self.S, S)
+			self.I = np.append(self.I, I)
+			self.M = np.append(self.M, M)
+			self.D = np.append(self.D, D)
+		
+			self.time = np.append(self.time, tmax)
+		
+			# Append interpolator
+			if self.interpolator:
+				ts = np.append(self.interpolator.ts, solutions.sol.ts[1:])
+				interpolants = self.interpolator.interpolants + solutions.sol.interpolants
+				self.interpolator = OdeSolution(ts, interpolants)
+			else: 
+				self.interpolator = solutions.sol

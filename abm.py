11f2cd3d931dc8d363a394481a70b9e167983ec2
Slater <tbs204@exeter.ac.uk>
2026-02-03 11:41:33 +0000

    Added daily_step function, changed distance to relative distance and changed initial preference to a weighted sum.

--- abm.py
+++ abm.py
@@ -9,24 +9,31 @@ class AgentBasedModel:
 
 	def __init__(self, parameters, seed):
 
+		# Parameters
+		if sum(parameters['preference_weights'])=1:
+			self.pref_weights = parameters['preference_weights']
+		else:
+			raise ValueError('Weights must sum to 1.')
+			break
+		
 		# Dictionary-based configuration (named distributions)
-		config_dict = {
+		config = {
 			'agent_probabilities': {
 				'class_name': 'Uniform',
 				'params': {'low': 0, 'high': 1}
 			}, 
-			'distance_from_gym': {
-				'class_name': 'Gamma',
-				'params': {'alpha': 2.5, 'beta': 3.5}
+			'relative_distance': {
+				'class_name': 'Beta',
+				'params': {'alpha': 4, 'beta': 2}
 			}
 		}
 		
 		# Create all distributions with a master seed
-		distributions = DistributionRegistry.create_batch(config_dict, main_seed=seed)
+		distributions = DistributionRegistry.create_batch(config, main_seed=seed)
 		
 		# Access distributions by name
 		self.probs_dist = distributions['agent_probabilities']
-		self.distance_dist = distributions['distance_from_gym']
+		self.distance_dist = distributions['relative_distance']
 
 	def generate_agents(self, max_agents):
 
@@ -42,17 +49,27 @@ class AgentBasedModel:
 		# Create agents
 		for i in range(self.num_agents):
 			attributes = {
-				'id': i
-				'prob': agent_probs[i],
+				'id': i,
+				'pref': 0.9*agent_probs[i] + 0.1*agent_distances[i],
 				'distance': agent_distances[i]
 			}
 			self.agent_list.append(Agent(attributes))
 
 		# Generate friendship network
-		social_network = nx.newman_watts_strogartz_graph(self.num_agents, 2, 0.1)
+		social_network = nx.newman_watts_strogatz_graph(self.num_agents, 2, 0.75)
 		agent_map = dict(zip(range(self.num_agents), self.agent_list))
 		nx.relabel_nodes(social_network, agent_map, copy=False)
+		nx.display(social_network, node_size=10, node_label=False, edge_label=False,
+				   node_alpha=0.8,edge_alpha=0.8, edge_width=0.5)
 
 		# Store friends as an agent attribute
 		for i in self.agent_list:
 			i.add_friends([j for j in social_network.neighbors(i)])
+
+	def daily_step(self):
+
+		for agent in agent_list:
+			new_pref = self.pref_weights[0] * agent.pref[0] + \
+			self.pref_weights[1] * agent.pref[2] + \ 
+			self.pref_weights[2] * np.mean([x.member for x in agent.friends])
+			agent.pref = np.append(agent.pref, new_pref)

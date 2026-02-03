45de948155ac1bc90880843585976980dd7f99a8
Slater <tbs204@exeter.ac.uk>
2026-02-03 11:39:50 +0000

    Added ex-member status and changed attribute name.

--- agent.py
+++ agent.py
@@ -6,8 +6,9 @@ class Agent:
 	def __init__(self, attributes):
 
 		self.member = 0
+		self.ex_member = 0
 		self.id = attributes['id']
-		self.prob = np.array([attributes['prob']])
+		self.pref = np.array([attributes['pref']])
 		self.distance = attributes['distance']
 
 	def add_friends(self, friends):

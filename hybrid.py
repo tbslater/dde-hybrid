64c771342d20ec46542f0d0b0c34d85ae830d683
Slater <tbs204@exeter.ac.uk>
2026-02-04 09:37:39 +0000

    Tidied method of changing agent state.

--- hybrid.py
+++ hybrid.py
@@ -15,9 +15,12 @@ class HybridSim(AgentBasedModel):
 		super().__init__(parameters, seeds[:1])
 
 		# Generators for randomly choosing agents for state change
-		self.member_rng = np.random.default_rng(seeds[2])
-		self.ex_member_rng = np.random.default_rng(seeds[3])
-		self.non_member_rng = np.random.default_rng(seeds[4])
+		self.converters = ['new_members', 'new_dropouts', 'new_potentials']
+		self.generators = {
+			self.converters[0]: np.random.default_rng(seeds[2]),
+			self.converters[1]: np.random.default_rng(seeds[3]),
+			self.converters[2]: np.random.default_rng(seeds[4])
+		}
 		
 		# Simulate until...
 		self.horizon = horizon
@@ -26,89 +29,56 @@ class HybridSim(AgentBasedModel):
 		self.sd_model = SystemDynamics(parameters)
 
 		# Change agent states in line with initial stock values
-		if self.sd_model.M[-1] != 0:
-			no_new_members = np.round(self.sd_model.M[-1])
-			possible_agents = [x in self.agent_list if x.member==0 and x.ex_member==0]
-			# Transform probs
-			probs = [x.pref for x in possible_agents]
-			transformed_probs = probs / sum(probs)
-			# Randomly choose new members
-			new_members = self.member_rng.choice(possible_agents, size=no_new_members,
-												replace=False, p=transformed_probs)
-			# Change states
-			for agent in new_members:
-				x.member = 1
-
-		if self.sd_model.D[-1] != 0:
-			no_new_dropouts = np.round(self.sd_model.D[-1])
-			possible_agents = [x in self.agent_list if x.member]
-			# Transform probs
-			probs = [x.pref for x in possible_agents]
-			transformed_probs = probs / sum(probs)
-			# Randomly choose new members
-			new_dropouts = self.member_rng.choice(possible_agents, size=no_new_dropouts,
-												replace=False, p=transformed_probs)
-			# Change states
-			for agent in new_dropouts:
-				x.dropout = 1
+		initial_vals = np.round([self.sd_model.M[-1], self.sd_model.D[-1]])
+		for i in range(2):
+			if initial_vals[i] != 0:
+				poss = [x in self.agent_list if x.member==i and x.dropout==0]
+				probs = [x.pref for x in possible_agents]
+				transformed_probs = probs / sum(probs)
+				to_change = \
+				self.generators[self.converters[i]].choice(poss, size=initial_vals[i],
+														  replace=False, p=transformed_probs)
+				for agent in to_change:
+					x.member == 1-i
+					x.dropout == i
 
 	def change_agent_states(self):
-		## This needs tidying!! ##
 
 		# Extract flows	
-		no_new_members = round(self.sd_model.P[-2] - self.sd_model.P[-2] + \
-		self.sd_model.N[-1])
-		no_new_dropouts = round(self.sd_model.D[-1] - self.sd_model.D[-2] + \
-		self.sd_model.N[-1])
-		no_new_potentials = round(self.sd_model.N[-1])
+		flow_vals = {
+			self.converters[0]: \ 
+			round(self.sd_model.P[-2] - self.sd_model.P[-2] + self.sd_model.N[-1]),
+			self.converters[1]: \
+			round(self.sd_model.D[-1] - self.sd_model.D[-2] + self.sd_model.N[-1]),
+			self.converters[2]: \
+			round(self.sd_model.N[-1])
+		}
 
-		# New members
-		if no_new_members != 0:
-			possible_agents = [x in self.agent_list if x.member==0 and x.ex_member==0]
-			# Transform probs
-			probs = [x.pref for x in possible_agents]
-			transformed_probs = probs / sum(probs)
-			# Randomly choose new members
-			new_members = self.member_rng.choice(possible_agents, 
-												 size=no_new_members,
-												 replace=False, 
-												 p=transformed_probs)
-			# Change states
-			for agent in new_members:
-				x.member = 1
+		for flow in enumerate(self.converters]):
+			if flow_vals[flow] != 0:
+				
+				if i==0:
+					possible = [x in self.agent_list if x.member==0 and x.dropout==0]
+				if i==1: 
+					possible = [x in self.agent_list if x.member]
+				if i==2:
+					possible = [x in self.agent_list if x.dropout]
+					
+				probs = [x.pref for x in possible_agents]
+				transformed_probs = probs / sum(probs)
+				
+				to_change = self.generators[flow].choice(possible, size=flow_vals[flow], 
+														 replace=False, p=transformed_probs)
 
-		# New dropouts
-		if no_new_dropouts != 0:
-			possible_agents = [x in self.agent_list if x.member]
-			# Transform probs
-			probs = [x.pref for x in possible_agents]
-			transformed_probs = probs / sum(probs)
-			# Randomly choose new members
-			new_dropouts = self.member_rng.choice(possible_agents, 
-												  size=no_new_dropouts,
-												  replace=False, 
-												  p=transformed_probs)
-			# Change states
-			for agent in new_dropouts:
-				x.member = 0
-				x.dropout = 1
+				for agent in to_change:
+					if i==0:
+						x.member = 1
+					if i==1:
+						x.member = 0
+						x.dropout = 1
+					if i==2:
+						x.dropout = 0
 
-		# New potential members
-		if no_new_potentials != 0:
-			possible_agents = [x in self.agent_list if x.dropout]
-			# Transform probs
-			probs = [x.pref for x in possible_agents]
-			transformed_probs = probs / sum(probs)
-			# Randomly choose new members
-			new_dropouts = self.member_rng.choice(possible_agents, 
-												  size=no_new_potentials,
-												  replace=False,
-												  p=transformed_probs)
-			
-			# Change states
-			for agent in new_dropouts:
-				x.member = 0
-				x.dropout = 0
 
 	def update_sd_parameters(self):
 
@@ -124,5 +94,4 @@ class HybridSim(AgentBasedModel):
 			self.daily_step()
 			self.change_agent_states()
 			self.update_sd_parameters()
-			self.sd_model.solve(t)
-			
+			self.sd_model.solve(t)		

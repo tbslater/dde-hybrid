a23449deb1e920e127bf60d81488bac4ba4a0beb
Slater <tbs204@exeter.ac.uk>
2026-02-24 09:43:46 +0000

    Removed deaths for simplicity.

--- hybrid.py
+++ hybrid.py
@@ -70,26 +70,6 @@ class HybridSim(AgentBasedModel, SystemDynamics):
 			
 			# Solve SD equations
 			self.solve(t)
-
-			# Calculate new deaths and adapt social network if necessary
-			new_deaths = int(self.D[-1]) - int(self.D[-2])
-			
-			if new_deaths > 0: # if any new deaths
-				
-				# Randomly sample agents
-				dead_agents = self.dead_generator.choice(self.agent_list,
-														 size=new_deaths,
-														 replace=False)
-				
-				# Remove dead agents from friendship list
-				for x in dead_agents:
-					for friend in x.friends:
-						if x in friend.friends:
-							friend.friends.remove(x)
-							friend.num_friends -= 1
-							
-				# Remove from network graph
-				self.social_network.remove_nodes_from(dead_agents)
 			
 			# Run one step of the ABM
 			self.daily_step(self.I[-1])
